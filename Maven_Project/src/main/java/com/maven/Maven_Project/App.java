51f43ab831511a027e6ae504cb4770c8c52bf498
Pensionthieng Hrangchal <87723912+PesoHrangchal@users.noreply.github.com>
2022-09-21 13:09:15 +0530

    Add files via upload

---
+++ Maven_Project/src/main/java/com/maven/Maven_Project/App.java
@@ -0,0 +1,13 @@
+package com.maven.Maven_Project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello! My name is Pensionthieng Hrangchal." );
+    }
+}
